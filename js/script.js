e13400dff778e41ee70567c4f9ff61663ecbbb2b
daniel iabanji <daniel.iabanji@yahoo.com>
2022-11-04 12:52:57 +0100

    milestone 5

--- js/script.js
+++ js/script.js
@@ -8,6 +8,58 @@ const app = createApp({
             currentChat: 0,
             newMessage: "",
             searchTerm: "",
+            showChat: false,
+            showemoji: false,
+            msgOpt: {
+                i: null,
+                show: false
+            },
+            icons: [
+                '&#128512;',
+                '&#128513;',
+                '&#128514;',
+                '&#128515;',
+                '&#128516;',
+                '&#128517;',
+                '&#128518;',
+                '&#128519;',
+                '&#128521;',
+                '&#128522;',
+                '&#128523;',
+                '&#128524;',
+                '&#128525;',
+                '&#128526;',
+                '&#128528;',
+                '&#128529;',
+                '&#128530;',
+                '&#128536;',
+                '&#128545;',
+                '&#128548;',
+                '&#128561;',
+                '&#129315;',
+                '&#129299;',
+                '&#129319;',
+                '&#129321;',
+                '&#129325;',
+                '&#129488;',
+                '&#9996;',
+                '&#9995;',
+                '&#10024;',
+                '&#10062;',
+                '&#128064;',
+                '&#128077;',
+                '&#128079;',
+                '&#128152;',
+                '&#128149;',
+                '&#128156;',
+                '&#128158;',
+                '&#128584;',
+                '&#129310;',
+                '&#129505;',
+                '&#127802;',
+                '&#127801;',
+                '&#9749;'
+            ],
             contacts: [
                 {
                     id: 1,
@@ -19,19 +71,16 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 },
@@ -45,19 +94,16 @@ const app = createApp({
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received',
-                            options: false,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent',
-                            options: false,
                         }
                     ],
                 },
@@ -71,19 +117,16 @@ const app = createApp({
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received',
-                            options: false,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 },
@@ -97,13 +140,11 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 },
@@ -117,13 +158,11 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 },
@@ -137,19 +176,16 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent',
-                            options: false,
                         }
                     ],
                 },
@@ -163,13 +199,11 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 },
@@ -183,19 +217,16 @@ const app = createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent',
-                            options: false,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
                             status: 'received',
-                            options: false,
                         }
                     ],
                 }
@@ -216,42 +247,48 @@ const app = createApp({
                         date: newdate,
                         message: this.newMessage,
                         status: 'sent',
-                        options: false,
                     }
                 );
-
-                setTimeout(this.messageReceived, 1000);
+                setTimeout(() => {
+                    const d = new Date();
+                    let newdate = d.toLocaleString('it-IT');
+                    this.contacts[this.currentChat].messages.push(
+                        {
+                            date: newdate,
+                            message: 'Ok!',
+                            status: 'received',
+                        }
+                    );
+                    this.$nextTick(() => {
+                        console.log(this.$refs.msg[this.$refs.msg.length - 1]);
+                        const el = this.$refs.msg[this.$refs.msg.length - 1];
+                        el.scrollIntoView();
+                    });
+                }, 1000);
             }
-
             this.newMessage = '';
         },
 
-        messageReceived() {
-            const d = new Date();
-            let newdate = d.toLocaleString('it-IT');
-            this.contacts[this.currentChat].messages.push(
-                {
-                    date: newdate,
-                    message: 'Ok!',
-                    status: 'received',
-                    options: false,
-                }
-            );
-        },
-        
-        getLastMessage(contact){
-            const msg = contact.messages.filter((message)=> {
-                return message.status === 'received';
-            })
-            return msg[msg.length - 1];
-        },
-        toggleClass(i){
-            this.contacts[this.currentChat].messages[i].options = !this.contacts[this.currentChat].messages[i].options
-        },
-        deleteMessage(i){
+        deleteMessage(i) {
             this.contacts[this.currentChat].messages.splice(i, 1);
-            this.toggleClass(i);
-        }
+            this.showOption(i);
+        },
+        showOption(i) {
+            if (i === this.msgOpt.i && this.msgOpt.show) {
+                this.msgOpt.i = null;
+                this.msgOpt.show = false
+            } else {
+                this.msgOpt.i = i;
+                this.msgOpt.show = true
+            }
+
+        },
+        showEmoji() {
+            this.showemoji = !this.showemoji;
+        },
+        addEmoji(i) {
+            this.newMessage += this.icons[i];
+        },
     },
     computed: {
         filteredConatact() {
@@ -259,7 +296,7 @@ const app = createApp({
                 const name = contact.name.toLowerCase();
                 return name.includes(this.searchTerm.toLowerCase());
             })
-            
+
         }
 
     },
